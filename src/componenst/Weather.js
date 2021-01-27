6d37f9490a750820583fb45c71698248585d5128
wala <walax31009@gmail.com>
2021-01-27 14:10:26 +0700

    EDIT function

--- src/componenst/Weather.js
+++ src/componenst/Weather.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import styled from 'styled-components'
 import { Form } from 'semantic-ui-react'
 import CardTemp from './CardTemp'
 import SegmentTemp from './SegmentTemp'
+import {fetchData} from '../service/FetchData'
 
 const Container = styled.div`
   width:50vw;
@@ -16,56 +17,14 @@ const WrapperContent = styled.div`
 `
 function Weather() {
   const [weather, setWeather] = useState([]);
-  const [form, setForm] = useState({
-    city: "",
-    country: ""
-  });
+  
+  useEffect(() => {
+    fetchData().then((data) => setWeather([data]))
+  })
 
-  async function weatherData(e) {
-    e.preventDefault();
-    if (form.city === "" || form.country === "") {
-      alert("Add value");
-    } else {
-      const data = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&APPID=67a2f61c4d2f058299ed454b400c5b29`
-      )
-        .then((res) => res.json(res))
-        .then((data) => setWeather([data]))
-    }
-
-  }
-  const handleInputChange = (event) => {
-    let name = event.target.name;
-    let value = event.target.value;
-
-    if (name === "city") {
-      setForm({ ...form, city: value });
-    }
-    if (name === "country") {
-      setForm({ ...form, country: value });
-    }
-  };
-
-  console.log(form.city, form.country);
   return (
     <Container>
-      <Form onSubmit={weatherData}>
-        <Form.Group>
-          <Form.Input
-            type="text"
-            name="city"
-            onChange={(e) => handleInputChange(e)}
-            placeholder="city"
-          />
-          <Form.Input
-            type="text"
-            name="country"
-            onChange={(e) => handleInputChange(e)}
-            placeholder="country"
-          />
-          <Form.Button content='Submit' />
-        </Form.Group>
-      </Form>
+     
       <WrapperContent>
         {weather != undefined ? console.log(weather[0]) : console.log('not fetch')}
         {weather != undefined ? (
